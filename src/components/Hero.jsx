c2a261c4fca0f3ba493e4d93b404c3d5e478e977
chengivan <chengoleivan@gmail.com>
2024-02-18 00:20:44 +0300

    Cleared README. Added skeleton for hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../componentstyles/Hero.css"
+import ivanthedevBot from '../assets/images/ivan-the-dev-bot.svg'
+
+export default function Hero() {
+  return (
+    <div className='hero-section'>
+        <div className="hero-section-columns">
+            <div className="hero-section-column-one">
+
+            </div>
+            <div className="hero-section-column-two">
+
+            </div>
+        </div>
+    </div>
+  )
+}
