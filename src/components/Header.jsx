b7e09109b1aeef143d1570387741ba4a44ab925e
chengivan <chengoleivan@gmail.com>
2024-02-17 22:13:29 +0300

    Header added. Other component files and images added

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import '../componentstyles/Header.css'
+import ivanLogo from '../assets/images/ivan-the-dev-logo-svg.svg'
+
+export default function Header() {
+  return (
+    <div className='header'>
+        <div className="header-content-pill">
+            <div className="header-logo">
+                <a href="http://"><img src={ivanLogo} alt="Ivan the Dev logo" /></a>
+            </div>
+
+            <div className="header-main-menu">
+                <ul>
+                    <li><a href="http://">Home</a></li>
+                    <li><a href="http://">Projects</a></li>
+                    <li><a href="http://">Contact</a></li>
+                    <li><a href="http://">About</a></li>
+                </ul>
+            </div>
+
+            <div className="header-cta-button">
+                <div className="header-cta-button-link">
+                    <a href="http://">Get Started</a>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
