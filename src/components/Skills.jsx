1e69aa2fd66c83cd56d1411a820275c2da61a0e7
chengivan <chengoleivan@gmail.com>
2024-02-19 06:24:47 +0300

    Added skills section skeleton

---
+++ src/components/Skills.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Skills() {
+  return (
+    <div className="skills-section">
+        <div className="skills-section-header">
+            <h2>Skills and Toolset</h2>
+        </div>
+
+        <div className="skills-section-paragraph-hook"></div>
+    </div>
+  )
+}
